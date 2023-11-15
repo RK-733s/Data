e1fac5e142ff38868de3990ed6451c3af6da0f65
RAJKUMAR <121594446+RK-733s@users.noreply.github.com>
2023-11-15 17:08:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 df=pd.read_csv("data.csv")
 st.title("RAJKUMAR")
+if st.sidebar.button("load dataset"):
+    st.write(df)
+    
+if st.sidebar.button("show graph"):
+    df2=df.head()
+    fig=plt.figure(figsize=(10,8))
+    plt.bar(df2['sl'],df2["trade"])
+    st.pyplot(fig)
+
+if st.sidebar.button("scatter plot"):
+    df2=df.head()
+    fig=plt.figure(figsize=(10,8))
+    plt.scatter(df2['sl'],df2["trade"])
+    plt.xlabel("sl",fontsize=15)
+    plt.ylabel("trade",fontsize=15)
+    st.pyplot(fig)
