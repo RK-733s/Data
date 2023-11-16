1b2c6318ae960259b6b732e83162003888c1fbc2
RAJKUMAR <121594446+RK-733s@users.noreply.github.com>
2023-11-16 16:18:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ st.title("RAJKUMAR")
 if st.sidebar.button("load dataset"):
     st.write(df)
     
-if st.sidebar.button("show graph"):
-    df2=df.head()
-    fig=plt.figure(figsize=(10,8))
-    plt.bar(df2['S No'],df2["Trade"])
-    st.pyplot(fig)
+# if st.sidebar.button("show graph"):
+#     df2=df.head()
+#     fig=plt.figure(figsize=(10,8))
+#     plt.bar(df2['S No'],df2["Trade"])
+#     st.pyplot(fig)
 
-if st.sidebar.button("scatter plot"):
-    df2=df.head()
-    fig=plt.figure(figsize=(10,8))
-    plt.scatter(df2['S No'],df2["Trade"])
-    plt.xlabel("S No",fontsize=15)
-    plt.ylabel("Trade",fontsize=15)
-    st.pyplot(fig)
+# if st.sidebar.button("scatter plot"):
+#     df2=df.head()
+#     fig=plt.figure(figsize=(10,8))
+#     plt.scatter(df2['S No'],df2["Trade"])
+#     plt.xlabel("S No",fontsize=15)
+#     plt.ylabel("Trade",fontsize=15)
+#     st.pyplot(fig)
